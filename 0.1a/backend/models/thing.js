39da8b05622cdd4b55606a8ddd93e06f01018de1
Jerome-G78 <67152285+Jerome-G78@users.noreply.github.com>
2020-11-16 14:18:43 +0100

    Update

--- 0.1a/backend/models/thing.js
+++ 0.1a/backend/models/thing.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // nous créons un schéma de données qui contient les champs souhaités pour chaque Thing, indique leur type ainsi que leur caractère (obligatoire ou non).
+
 const thingSchema = mongoose.Schema({
   // _id est créer automatiquement 
   name: { type: String, required: true },
@@ -8,8 +9,7 @@ const thingSchema = mongoose.Schema({
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   heat: { type: Number, required: true },
-  userId: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true }
 });
 
 // nous exportons ce schéma en tant que modèle Mongoose appelé « Thing », le rendant par là même disponible pour notre application Express.
