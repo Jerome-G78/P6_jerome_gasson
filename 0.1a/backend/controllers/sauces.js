69626606b4a207445b64a9fd8fef7ca4b3d9e563
Jerome-G78 <67152285+Jerome-G78@users.noreply.github.com>
2020-11-19 16:32:32 +0100

    Update

--- 0.1a/backend/controllers/sauces.js
+++ 0.1a/backend/controllers/sauces.js
@@ -32,9 +32,7 @@ exports.getOneSauces = (req, res, next) => {
     }
   ).catch(
     (error) => {
-      res.status(404).json({
-        error: error
-      });
+      res.status(404).json({error: error});
     }
   );
 };
@@ -65,15 +63,9 @@ exports.deleteSauces = (req, res, next) => {
 
 exports.getAllStuff = (req, res, next) => {
   Sauces.find().then(
-    (things) => {
-      res.status(200).json(things);
-    }
+    (things) => {res.status(200).json(things);}
   ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
+    (error) => {res.status(400).json({error: error});}
   );
 };
 
